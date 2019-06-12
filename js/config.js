c07b8ab1580bf4134a381728cb1d2f8c434450f3
Xavier Arques <xavarques@gmail.com>
2019-06-12 08:56:19 +0000

    Add cognito credentials

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_0xf5HfHHf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4mrd2lcuoj8q7f31d6jmsaeqd8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
